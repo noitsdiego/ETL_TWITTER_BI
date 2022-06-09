6afe962e1f3b89fb5f340a0203cdd3157e6ac1b5
Diego Andres Riveros Lopez <65474383+diegor06@users.noreply.github.com>
2022-06-09 13:49:51 -0500

    Acomodación de codigo e implementación de nuevos metodos
    Se crean nuevos metodos de segregación de a información, de igual forma, la estructura creada anteriormente se acomodó para su mejor utilización

--- twitter_apiConnect/twitter_init.py
+++ twitter_apiConnect/twitter_init.py
@@ -9,26 +9,68 @@ api_key_secret = ""
 access_token = ""
 access_token_secret = ""
 
+
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-tweets_search = api.search_tweets("Colombia", tweet_mode="extended")
-
-columnas = ['USUARIO','RT','VERFICACION','SEGUIDORES','TWEETS REALIZADOS',
-            'TWEET ACTUAL','REALIZADO EN','ES RESPUESTA', 'LUGAR','NUMERO DE RETWEET','NUMERO DE LIKES']
-data = []
-
-for tweet in tweets_search:
+def buscarTwetts(tema):
+    tweets_search = api.search_tweets(tema, tweet_mode="extended")
     
+    columnas = ['USUARIO','VERFICACION','SEGUIDORES','TWEETS REALIZADOS',
+            'TWEET ACTUAL','REALIZADO EN','NUMERO DE RETWEET','NUMERO DE LIKES']
+    data = []
+    
+    for tweet in tweets_search:
+        texto = limpiarTexto(tweet.full_text)
+        data.append([tweet.user.screen_name, tweet.user.verified , tweet.user.followers_count, tweet.user.statuses_count, 
+                 texto, tweet.source, tweet.retweet_count, tweet.favorite_count])
+    
+    return pd.DataFrame(data, columns=columnas)
+
+def obtenerTweetRT(dfTotal):
+    columnas = ['USUARIO','VERFICACION','SEGUIDORES','TWEETS REALIZADOS',
+            'TWEET ACTUAL','REALIZADO EN','NUMERO DE RETWEET','NUMERO DE LIKES']
+    data = []
+    for i in range(len(dfTotal)): 
+        if((dfTotal.loc[i,"TWEET ACTUAL"]).__contains__('RT @')):
+            data.append(dfTotal.loc[i])
+    return  pd.DataFrame(data, columns=columnas) 
+
+def obtenerTweetVerficados(dfTotal):
+    columnas = ['USUARIO','VERFICACION','SEGUIDORES','TWEETS REALIZADOS',
+            'TWEET ACTUAL','REALIZADO EN','NUMERO DE RETWEET','NUMERO DE LIKES']
+    data = []
+    for i in range(len(dfTotal)): 
+        if(str(dfTotal.loc[i,"VERFICACION"]).__contains__('True')):
+            data.append(dfTotal.loc[i])
+    return  pd.DataFrame(data, columns=columnas)   
+
+def obtenerTweetNoVerficados(dfTotal):
+    columnas = ['USUARIO','VERFICACION','SEGUIDORES','TWEETS REALIZADOS',
+            'TWEET ACTUAL','REALIZADO EN','NUMERO DE RETWEET','NUMERO DE LIKES']
+    data = []
+    for i in range(len(dfTotal)): 
+        if(str(dfTotal.loc[i,"VERFICACION"]).__contains__('False')):
+            data.append(dfTotal.loc[i])
+    return  pd.DataFrame(data, columns=columnas)     
+
+def obtenerTweetOrginal(dfTotal):
+    columnas = ['USUARIO','VERFICACION','SEGUIDORES','TWEETS REALIZADOS',
+            'TWEET ACTUAL','REALIZADO EN','NUMERO DE RETWEET','NUMERO DE LIKES']
+    data = []
+    for i in range(len(dfTotal)): 
+        if(not (dfTotal.loc[i,"TWEET ACTUAL"]).__contains__('RT @')):
+            data.append(dfTotal.loc[i])
+    return  pd.DataFrame(data, columns=columnas)            
+             
+
+def limpiarTexto(texto):
     texto = re.sub(
         r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
-        normalize( "NFD", tweet.full_text), 0, re.I
+        normalize( "NFD", texto), 0, re.I
     )
-    
-    if(texto.__contains__('@') and texto.__contains__('RT') and texto.__contains__(':')) :
-        texto = texto.split(':')[1]
         
     texto = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', texto)
     texto = re.sub(r"  +", ' ', texto)
@@ -42,12 +84,11 @@ for tweet in tweets_search:
     
     texto = emoji_pattern.sub(r'', texto)
     texto = re.sub(r'[^\s][...]+', '',texto)
-
-    data.append([tweet.user.screen_name, tweet.is_quote_status, tweet.user.verified , tweet.user.followers_count, tweet.user.statuses_count, 
-                 texto, tweet.source, tweet.in_reply_to_status_id, tweet.place, tweet.retweet_count, tweet.favorite_count])
-    
-    
-df = pd.DataFrame(data, columns=columnas)
+    return texto
 
 
-df.to_csv('tweet.csv', sep=';')
+dfCentral = buscarTwetts("#ColombiaDecide");
+dfObtenerRT = obtenerTweetRT(dfCentral)
+dfTweetOriginal = obtenerTweetOrginal(dfCentral)
+dfTweetDeVerificados = obtenerTweetVerficados(dfCentral)
+dfTweetDeNoVerificados = obtenerTweetNoVerficados(dfCentral)
