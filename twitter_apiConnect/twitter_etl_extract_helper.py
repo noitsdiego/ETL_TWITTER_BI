9795de5593f9dbaaf1522cd5f9f2f6f9e35a984d
Diego Andres Riveros Lopez <65474383+diegor06@users.noreply.github.com>
2022-06-09 22:43:56 -0500

    llaves del api de twitter and persistencia en csv
    Se realiza la persistencia en archivos csv y se agregan las llaves para el acceso a la api.

--- twitter_apiConnect/twitter_etl_extract_helper.py
+++ twitter_apiConnect/twitter_etl_extract_helper.py
@@ -1,10 +1,10 @@
 import tweepy
 
-api_key = ""
-api_key_secret = ""
+api_key = "Ip9pdpfh8UHnEss1JkWHrKDRZ"
+api_key_secret = "TZA1066NoRZBPczJsRK7IZ0adxZVHsU6rhIuFW26xLAvpt74bx"
 
-access_token = ""
-access_token_secret = ""
+access_token = "1350080036-A2GKe8RnEdmwdwjmw0RVmuchN6qzvo55ViZgJmR"
+access_token_secret = "hszrFxwzN2DUCVOxLNxeYawi2CaaethCHiJqhEVoSX2HC"
 
 def extraerDatosDesdeBusqueda(tema):
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
--- twitter_apiConnect/twitter_init.py
+++ twitter_apiConnect/twitter_init.py
@@ -3,8 +3,9 @@ import twitter_etl_transform_helper
 import twitter_etl_load_helper
 
 
-
 tweets_search = twitter_etl_extract_helper.extraerDatosDesdeBusqueda("#EleccionesColombia2022")
 dfTweets = twitter_etl_transform_helper.obtenerTweets(tweets_search)
+
+dfTweets.to_csv("Alltweet.json", sep=";");
 
 twitter_etl_load_helper.guardarBaseDeDatos(dfTweets,"tweets")
