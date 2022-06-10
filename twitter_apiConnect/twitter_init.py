fc0a7dfd98236099006a4d80383b68a601101058
proteuz <jenunezb@uqvirtual.edu.co>
2022-06-09 23:12:59 -0500

    Config

--- twitter_apiConnect/twitter_init.py
+++ twitter_apiConnect/twitter_init.py
@@ -6,6 +6,6 @@ import twitter_etl_load_helper
 tweets_search = twitter_etl_extract_helper.extraerDatosDesdeBusqueda("#EleccionesColombia2022")
 dfTweets = twitter_etl_transform_helper.obtenerTweets(tweets_search)
 
-dfTweets.to_csv("Alltweet.json", sep=";");
+dfTweets.to_csv("Alltweet.csv", sep=";");
 
 twitter_etl_load_helper.guardarBaseDeDatos(dfTweets,"tweets")
