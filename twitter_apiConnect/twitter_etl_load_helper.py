ffc147e72f3364e78e931e710ad8855598b8744c
Diego Andres Riveros Lopez <65474383+diegor06@users.noreply.github.com>
2022-06-10 18:34:09 -0500

    creacion de lambda y persistir en AWS
    Se crea la lambda necesaria y un metodo especializado en guardar la información en una RDS en AWS

--- twitter_apiConnect/twitter_etl_load_helper.py
+++ twitter_apiConnect/twitter_etl_load_helper.py
@@ -11,3 +11,16 @@ def guardarBaseDeDatos(df, nameTable):
     db_version = cur.fetchone()
     print(db_version)
     cur.close()
+    
+    
+def guardarBaseDeDatosAWS(df, nameTable):
+    conn = psycopg2.connect(host="etl-twitter.cgyicsjyl7q5.sa-east-1.rds.amazonaws.com", port = "5432", dbname = "etltwitter"
+                            , user = "postgres", password = "admin1234")
+    cur = conn.cursor()
+    engine = create_engine('postgresql://postgres:%s@etl-twitter.cgyicsjyl7q5.sa-east-1.rds.amazonaws.com:5432/etltwitter'
+                           %urlquote('admin1234'))
+    df.to_sql(name = nameTable, con = engine, if_exists='append', index= False)
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    print(db_version)
+    cur.close()   
